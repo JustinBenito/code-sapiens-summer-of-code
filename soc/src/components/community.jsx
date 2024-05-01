9f217be6ddd6d0b41a94c519c8f886f78d4cfcc1
JustinBenito <justinbenito1974@gmail.com>
2024-05-01 16:01:54 +0530

    minor fix

--- soc/src/components/community.jsx
+++ soc/src/components/community.jsx
@@ -28,7 +28,7 @@ const Community = () => {
         radius={50}
         className="h-64"
       >
-        <div className="h-32 w-32">
+        <div className="h-24 w-24">
         <div id="tooltip-default" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
     Tooltip content
     <div class="tooltip-arrow" data-popper-arrow></div>
